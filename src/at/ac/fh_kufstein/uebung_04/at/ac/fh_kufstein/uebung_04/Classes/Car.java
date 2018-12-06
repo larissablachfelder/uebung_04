c4d9b8ae21c525ebe7ce1f46f05028f221765b91
Larissa Blachfelder <1810653996@stud.fh-kufstein.ac.at>
2018-12-06 15:09:56 +0100

    aufgabe 3

--- src/at/ac/fh_kufstein/uebung_04/at/ac/fh_kufstein/uebung_04/Classes/Car.java
+++ src/at/ac/fh_kufstein/uebung_04/at/ac/fh_kufstein/uebung_04/Classes/Car.java
@@ -12,11 +12,40 @@ public class Car
         System.out.println(speed);
     }
 
+    //aufgabe  2
     //Konstruktor erstellt der direkt die Variablen aufnimmt
     public Car (String color, int wheels, double speed)
     {
         this.color = color;
         this.wheels = wheels;
         this.speed = speed;
+    }
+
+    //aufgabe 3 --> methoden erweitern
+    public String getColor()
+    {
+        return color;
+    }
+
+    public int getWheels()
+    {
+        return wheels;
+    }
+
+    public double getSpeed()
+    {
+        return speed;
+    }
+
+    //erhöht die Geschwindigkeit um den übergebenen wert
+    public double accelerate(double geschwindigkeit)
+    {
+        return this.speed += geschwindigkeit;
+    }
+    //verringert die Geschwindigkeit um den übergebenen wert
+
+    public double slowdown(double geschwindigkeit)
+    {
+        return this.speed -= geschwindigkeit;
     }
 }
